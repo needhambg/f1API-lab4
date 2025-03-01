ca06895a9c088f6226b5c199e06c7a9a8f199398
needhambg <needhambg@etsu.edu>
2025-02-28 21:22:48 -0500

    Some Little Work On Menus etc

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,26 +1,25 @@
 import requests
 from pydantic import ValidationError
-from models import Meeting, Session
+from models import Meeting, Session, Position, Menu, DriverMenu
 base_api_url = "https://api.openf1.org/v1/"
-drivernum = int(input("Input a Driver Number: "))
-meeting = requests.get(f"{base_api_url}meetings?year=2023&circuit_short_name=Singapore")
-if meeting.status_code == 200:
-    data = meeting.json()
-    
-    if data:  
-        try:
-            singapore = Meeting(**data[0])
-            result = requests.get(f"{base_api_url}sessions?meeting_key={singapore.meeting_key}&session_type=Race")
-            print(result.json())
-            sess_data = result.json()
-            session = Session(**sess_data[0])
-            result2 = requests.get(f"{base_api_url}position?session_key={session.session_key}&driver_number={drivernum}")
-            data2 = result2.json()
-            objlst = data2[-1]
-            print(objlst)
-        except ValidationError as e:
-            print("Validation Error:", e)
-    else:
-        print("No meeting found.")
-else:
-    print
+loopnum = 0
+while loopnum != 1:
+    # \/ Add Try Catches for Each Option
+    #Menu
+    usrchc = Menu()
+    drvrchc = DriverMenu()
+    #drivernum = int(input("Input a Driver Number: "))
+    #meeting = requests.get(f"{base_api_url}meetings?year=2023&circuit_short_name=Singapore")
+    #data = meeting.json()
+    #singapore = Meeting(**data[0])
+    #result = requests.get(f"{base_api_url}sessions?meeting_key={singapore.meeting_key}&session_type=Race")
+    #sess_data = result.json()
+    #session = Session(**sess_data[0])
+    #result2 = requests.get(f"{base_api_url}position?session_key={session.session_key}&driver_number={drivernum}")
+    #data2 = result2.json()
+    #position = Position()
+    #print(position.position)
+    #Predict Drivers Next Race at A Circuit
+        #Give User List of Drivers,take input
+    #Find A Driver's Best Race, Quali, Sprint, Or Practice
+    #Print Last Two Races
--- lab4/models.py
+++ lab4/models.py
@@ -11,3 +11,48 @@ class Session(BaseModel):
 class Position(BaseModel):
     position:int
     session_key:int
+driverlist = {
+        1: "Max Verstappen",
+        2: "Logan Sargeant",
+        3: "Daniel Ricciardo",
+        4: "Lando Norris",
+        10: "Pierre Gasly",
+        11: "Sergio Perez",
+        14: "Fernando Alonso",
+        16: "Charles Leclerc",
+        18: "Lance Stroll",
+        20: "Kevin Magnussen",
+        22: "Yuki Tsunoda",
+        23: "Alexander Albon",
+        24: "Zhou Guanyu",
+        27: "Nico Hulkenberg",
+        30: "Liam Lawson",
+        31: "Esteban Ocon",
+        38: "Oliver Bearman",
+        43: "Franco Colapinto",
+        44: "Lewis Hamilton",
+        50: "Oliver Bearman",
+        55: "Carlos Sainz",
+        61: "Jack Doohan",
+        63: "George Russell",
+        77: "Valtteri Bottas",
+        81: "Oscar Piastri"
+    }
+def Menu():
+    print("-----------F1 API-----------")
+    usrinput = int(input("1. Predict Drivers Next Race At A Circuit\n2. Find A Driver's Best At A Circuit\n3. Print Last Two Races\n--------------------------\n"))
+    return usrinput
+def DriverMenu():
+    drvrlpnum = 0
+    while drvrlpnum != 1:
+        print("----------Drivers----------")
+        for val in driverlist:
+            print(f"{val}: {driverlist[val]}")
+        print("---------------------------")
+        drivernum = int(input("Please Input A Driver Number: "))
+        if drivernum in driverlist:
+            drvrlpnum = 1
+            return drivernum
+        else:
+            print("Number Is Not In Drivers' List")
+    
