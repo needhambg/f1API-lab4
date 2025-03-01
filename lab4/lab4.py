c67ac9486b09ac596880de93b24fe3b4bc38dfc2
needhambg <needhambg@etsu.edu>
2025-03-01 17:30:04 -0500

    First Piece of Menu Done!

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,6 +1,6 @@
 import requests
 from pydantic import ValidationError
-from models import Meeting, Session, Position, Menu, DriverMenu, CircuitMenu
+from models import Meeting, Session, Position, Menu, DriverMenu, CircuitMenu,driverlist
 base_api_url = "https://api.openf1.org/v1/"
 loopnum = 0
 while loopnum != 1:
@@ -12,24 +12,34 @@ while loopnum != 1:
     #Switch Case
     match usrchc:
         case 1:
-            #1. Predict Drivers Next Race At A Circuit 
-            crctchc = CircuitMenu()
-            meeting = requests.get(f"{base_api_url}meetings?year=2023&circuit_short_name={crctchc}")
-            meeting2 = requests.get(f"{base_api_url}meetings?year=2024&circuit_short_name={crctchc}")
-            data = meeting.json()
-            r1 = Meeting(**data[0])
-            print(r1.meeting_key)
-            data2 = meeting2.json()
-            r2 = Meeting(**data2[0])
-            print(r2.meeting_key)
-            #race = Meeting(**data[0])
-            #result = requests.get(f"{base_api_url}sessions?meeting_key={race.meeting_key}&session_type=Race")
-            #sess_data = result.json()
-            #session = Session(**sess_data[0])
-            #result2 = requests.get(f"{base_api_url}position?session_key={session.session_key}&driver_number={drvrchc}")
-            #position = Position()
-            #print(position.position)
-            
+            try:
+                #1. Predict Drivers Next Race At A Circuit 
+                crctchc = CircuitMenu()
+                meeting = requests.get(f"{base_api_url}meetings?year=2023&meeting_name={crctchc}")
+                meeting2 = requests.get(f"{base_api_url}meetings?year=2024&meeting_name={crctchc}")
+                if meeting.status_code == 200:
+                    data = meeting.json()
+                    r1 = Meeting(**data[0])
+                    data2 = meeting2.json()
+                    r2 = Meeting(**data2[0])
+                    s1 = requests.get(f"{base_api_url}sessions?meeting_key={r1.meeting_key}&session_type=Race")
+                    s2 = requests.get(f"{base_api_url}sessions?meeting_key={r2.meeting_key}&session_type=Race")
+                    sesjson = s1.json()
+                    sesjson2 = s2.json()
+                    session1 = Session(**sesjson[0])
+                    session2 = Session(**sesjson[0])
+                    p1 = requests.get(f"{base_api_url}position?session_key={session1.session_key}&driver_number={drvrchc}")
+                    pos1 = p1.json()
+                    position1 = Position(**pos1[-1])
+                    p2 = requests.get(f"{base_api_url}position?session_key={session2.session_key}&driver_number={drvrchc}")
+                    pos2 = p2.json()
+                    position2 = Position(**pos2[-1])
+                    predpos = round((position1.position + position2.position)/2)
+                    print(f"{driverlist[drvrchc]} is predicted to place {predpos} at {crctchc} 2025!")
+                else:
+                    print("Request failed. Status Code:",meeting.status_code)
+            except ValidationError:
+                print("Invalid Input. Please Try Again")
         case 2:
             #2. Find A Driver's Best At A Circuit
             print
