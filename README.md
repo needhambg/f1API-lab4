c6d54f8fbcae5318f71793c9e40539758a9bc0e7
needhambg <needhambg@etsu.edu>
2025-03-02 13:31:08 -0500

    Some Changes to Models and Bug Fixes

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 #What It Does
 
 ##ISSUES
+#I had a little problem with how the API responded with JSON for the positions as it returns the last position of each lap. 
+#I had an error with status code returns and it just started working again for no reason
+#I had made an error in the circuit list and fixed it to use the meeting_name rather than circuit_short_name
--- lab4/lab4.py
+++ lab4/lab4.py
@@ -6,9 +6,11 @@ loopnum = 0
 while loopnum != 1:
     # \/ Add Try Catches for Each Option
     #Menu
-    usrchc = Menu()
-    drvrchc = DriverMenu()
-    
+    try:
+        usrchc = Menu()
+        drvrchc = DriverMenu()
+    except Exception as e:
+        print(f"Entered Value Must Be an Integer: Exception | {e} | Thrown")
     #Switch Case
     match usrchc:
         case 1:
@@ -17,7 +19,7 @@ while loopnum != 1:
                 crctchc = CircuitMenu()
                 meeting = requests.get(f"{base_api_url}meetings?year=2023&meeting_name={crctchc}")
                 meeting2 = requests.get(f"{base_api_url}meetings?year=2024&meeting_name={crctchc}")
-                if meeting.status_code == 200:
+                if meeting.status_code == 200 and meeting2.status_code == 200:
                     data = meeting.json()
                     r1 = Meeting(**data[0])
                     data2 = meeting2.json()
@@ -37,12 +39,30 @@ while loopnum != 1:
                     predpos = round((position1.position + position2.position)/2)
                     print(f"{driverlist[drvrchc]} is predicted to place {predpos} at {crctchc} 2025!")
                 else:
-                    print("Request failed. Status Code:",meeting.status_code)
+                    print("Request failed. Status Code:",meeting.status_code,meeting2.status_code)
             except ValidationError:
                 print("Invalid Input. Please Try Again")
         case 2:
             #2. Find A Driver's Best At A Circuit
             print
+            if raceposition1.position < raceposition2.position:
+                    print(f"{driverlist[drvrchc]}")
         case 3:
             #3. Print Last Two Races
-            print
+            #Last meeting key is 1252
+            try:
+                race1 = requests.get(f"{base_api_url}sessions?meeting_key=1252&session_type=Race")
+                race2 = requests.get(f"{base_api_url}sessions?meeting_key=1251&session_type=Race")
+                rd1 = race1.json()
+                rd2 = race2.json()
+                d1 = Session(**rd1[0])
+                d2 = Session(**rd2[0])
+                r1posreq = requests.get(f"{base_api_url}position?session_key={d1.session_key}&driver_number={drvrchc}")
+                r2posreq = requests.get(f"{base_api_url}position?session_key={d2.session_key}&driver_number={drvrchc}")
+                r1posjson = r1posreq.json()
+                r2posjson = r2posreq.json()
+                raceposition1 = Position(**r1posjson[-1])
+                raceposition2 = Position(**r2posjson[-1])
+                print(f"Most Recent: {driverlist[drvrchc]} | {d1.circuit_short_name} | Position: {raceposition1.position}\nPrevious: {driverlist[drvrchc]} | {d2.circuit_short_name} | Position: {raceposition2.position}")
+            except Exception as e:
+                print(f"Error Ocurred. Exception: {e}")
